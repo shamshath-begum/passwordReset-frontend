11572fc36d66eb57a887953a0861354fcb03099b
shamshath-begum <sham29.b@gmail.com>
2023-03-23 05:55:40 -0700

    hi

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import './App.css';
-import Header from './components/Header';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import Passwordreset from './components/Passwordreset';
-import Forgotpassword from './components/Forgotpassword';
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Passwordreset from "./components/Passwordreset";
+import Forgotpassword from "./components/Forgotpassword";
 
-export const url='http://localhost:8000'
-
+export const url = "https://passwordreset-lxax.onrender.com";
 
 function App() {
-  return <>
-  <BrowserRouter>
-  <Header/>
-  <Routes>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/signup' element={<SignUp/>} />
-    <Route path='/passwordreset' element={<Passwordreset/>} />
-    <Route path='/forgotpassword/:id/:token' element={<Forgotpassword/>} />
-    <Route path='*' element={<Navigate to='/login' />} />
-  </Routes>
-  </BrowserRouter>
-  </>
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/passwordreset" element={<Passwordreset />} />
+          <Route
+            path="/forgotpassword/:id/:token"
+            element={<Forgotpassword />}
+          />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
